17336ded68ca8e3c9f29aed8cac1e7ee2e0d6536
RakeGoose <rustem.aligozhin.99@mail.ru>
2025-04-16 15:50:18 +0500

    Completed Final

--- Assets/Scripts/LevelMusicManager.cs
+++ Assets/Scripts/LevelMusicManager.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LevelMusicManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private static LevelMusicManager instance;
+    private AudioSource audioSource;
+
+    void Awake()
     {
-        
+
+        if(instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        audioSource = GetComponent<AudioSource>();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        string sceneName = SceneManager.GetActiveScene().name.ToLower();
+
+        if (!IsGameplayScene(sceneName))
+        {
+            Destroy(gameObject);
+        }
+
+        var menuMusic = FindObjectOfType<MenuMusicPlayer>();
+        if(menuMusic != null)
+        {
+            var source = menuMusic.GetComponent<AudioSource>();
+            if(source != null)
+            {
+                source.Stop();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        
+        string sceneName = scene.name.ToLower();
+
+        if (!IsGameplayScene(sceneName))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private bool IsGameplayScene(string sceneName)
+    {
+        return sceneName.Contains("level") && !sceneName.Contains("select");
+    }
+
+    void OnDestroy()
+    {
+        if(instance = this)
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+            instance = null;
+        }
     }
 }
